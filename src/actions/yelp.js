143bac69b33730a5b4375116b0d757ac1451dcf3
Henry Lu (呂弈臻) <MTK13126@mediatek.inc>
2016-08-24 16:53:00 +0800

    CORS

--- src/actions/yelp.js
+++ src/actions/yelp.js
@@ -74,7 +74,7 @@ var n = require('nonce')();
 //   var instance = axios.create({
 //     headers: {'Access-Control-Allow-Origin': '*','Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS','Access-Control-Allow-Headers': 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token' }
 // });
-
+  console.log(axios);
   const request = axios.get(apiURL)
   // .then(function (response) {
   //   //{response.headers =  {'Access-Control-Allow-Origin': '*','Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS','Access-Control-Allow-Headers': 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token'}}
