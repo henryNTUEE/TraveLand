75c91a791d50e97748eac8ee588daf5c61fc0760
Henry Lu (呂弈臻) <MTK13126@mediatek.inc>
2016-08-24 15:29:04 +0800

    CORS

--- src/actions/yelp.js
+++ src/actions/yelp.js
@@ -72,11 +72,10 @@ var n = require('nonce')();
   console.log(apiURL);
 
   var instance = axios.create({
-    baseURL: apiURL,
     headers: {'Access-Control-Allow-Origin': '*','Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS','Access-Control-Allow-Headers': 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token' }
 });
 
-  instance.get()
+  instance.get(apiURL)
   .then(function (response) {
     {response.headers =  {'Access-Control-Allow-Origin': '*','Access-Control-Allow-Methods': 'PUT, GET, POST, DELETE, OPTIONS','Access-Control-Allow-Headers': 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token'}}
     
